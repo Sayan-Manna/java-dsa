1edfbf0fb69d7ce213ef9a6aca1ae0a70cb356b7
Sayan Manna <rexsayan028@gmail.com>
2025-07-09 00:00:01 +0530

    Auto-commit while IntelliJ active: 2025-07-09 00:00:01

---
+++ StackAndQueue/Basics/StackImplUsingArrays.java
@@ -0,0 +1,13 @@
+package StackAndQueue.Basics;
+
+import java.util.Stack;
+
+public class StackImplUsingArrays {
+    public static void main(String[] args) {
+       Stack st = new Stack()
+    }
+}
+class Stack {
+    int size
+
+}
