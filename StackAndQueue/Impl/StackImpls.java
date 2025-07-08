8b31e8ce020136a4c59279f4a65023cba5be2b7e
Sayan Manna <rexsayan028@gmail.com>
2025-07-09 01:43:00 +0530

    Auto-commit while IntelliJ active: 2025-07-09 01:43:00

--- StackAndQueue/Impl/StackImpls.java
+++ StackAndQueue/Impl/StackImpls.java
@@ -30,8 +30,35 @@ public class StackImpls {
             return top + 1;
         }
     }
-    public static class LinkedStack {
-        
+    public static class LinkedListStack {
+        // creating the stack node
+        private class stackNode {
+            int val;
+            stackNode next;
+            stackNode(int d) {
+                val = d;
+                next = null;
+            }
+        }
+        stackNode top;
+        int size;
+
+        stack() {
+            this.top = null;
+            this.size = 0;
+        }
+
+        void stackPush(int x) {
+            stackNode elemNode = new stackNode(x);
+            elemNode.next = top; // structure top -> [2] -> [1] -> null
+            top = elemNode;
+            System.out.println("pushed");
+            size++;
+        }
+        int stackPop() {
+
+
+        }
     }
 
 }
