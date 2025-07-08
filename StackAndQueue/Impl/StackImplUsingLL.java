66dc7c2db270838428a4d166903ef80b33ecc4ed
Sayan Manna <rexsayan028@gmail.com>
2025-07-09 03:00:00 +0530

    Auto-commit while IntelliJ active: 2025-07-09 03:00:00

--- StackAndQueue/Impl/StackImplUsingLL.java
+++ StackAndQueue/Impl/StackImplUsingLL.java
@@ -2,10 +2,5 @@ package StackAndQueue.Impl;
 
 public class StackImplUsingLL {
     StackImpls.LinkedListStack s = new StackImpls.LinkedListStack();
-    s.stackPush(10);
-    s.stackPush(20);
-    s.printStack();
-    System.out.println("Element popped " + s.stackPop());
-    System.out.println("Stack size: " + s.stackSize());
-    System.out.println("Stack is empty or not: " + s.stackIsEmpty());
+    StackImpls.LinkedListStack.stackNode
 }
