6a008e191b24eca23f0e0eca6b2988fb7fafc594
Sayan Manna <rexsayan028@gmail.com>
2025-07-09 01:14:01 +0530

    Auto-commit while IntelliJ active: 2025-07-09 01:14:01

--- StackAndQueue/Impl/StackImplUsingLL.java
+++ StackAndQueue/Impl/StackImplUsingLL.java
@@ -1,5 +1,18 @@
 package StackAndQueue.Impl;
 
 public class StackImplUsingLL {
+
+}
+class Stack {
+    // creating the stack node
+    private class stackNode {
+        int val;
+        stackNode next;
+        stackNode(int d) {
+            val = d;
+            next = null;
+        }
+    }
     
+
 }
