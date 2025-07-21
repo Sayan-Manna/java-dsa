a5a7ee6b8bd72381d093e181b94aeeb7cc791e99
Sayan Manna <rexsayan028@gmail.com>
2025-07-21 20:10:00 +0530

    Auto-commit while IntelliJ active: 2025-07-21 20:10:00

--- StackAndQueue/NextGreaterElem.java
+++ StackAndQueue/NextGreaterElem.java
@@ -17,15 +17,15 @@ public class NextGreaterElem {
         * So fix one check if any num > fixed curr no.
          */
         int[] nge = new int[arr.length];
-        boolean noMatch = true;
+        boolean isPresent = false;
         for (int i=0; i<arr.length-1; i++) {
             nge[i] = -1;
             for (int j=i+1; j<arr.length; j++) {
                 if (arr[j] > arr[i]) {
                     nge[i] = arr[j];
-                    noMatch = false;
+                    isPresent = true;
+                    break;
                 }
-                break;
             }
         }
         return nge;
