217c8e0b787aee3b55a5934ab213d66460901b7f
Sayan Manna <rexsayan028@gmail.com>
2025-07-21 19:58:01 +0530

    Auto-commit while IntelliJ active: 2025-07-21 19:58:01

--- StackAndQueue/NextGreaterElem.java
+++ StackAndQueue/NextGreaterElem.java
@@ -2,6 +2,23 @@ package StackAndQueue;
 
 public class NextGreaterElem {
     public static void main(String[] args) {
-        
+        int arr[]={5,7,1,2,6,0};
+
+        int arr2[] = nextGreaterElements(arr);
+        System.out.println("The next greater elements are ");
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+    }
+
+    private static int[] nextGreaterElements(int[] arr) {
+        // Brute-force
+        /*
+        * So fix one check if any num > fixed curr no.
+         */
+        for (int i=0; i<arr.length; i++) {
+            
+        }
+
     }
 }
