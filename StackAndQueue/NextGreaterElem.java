c22a1c5b60b3daa47a9de2438a56b62e154ce575
Sayan Manna <rexsayan028@gmail.com>
2025-07-21 20:06:01 +0530

    Auto-commit while IntelliJ active: 2025-07-21 20:06:01

--- StackAndQueue/NextGreaterElem.java
+++ StackAndQueue/NextGreaterElem.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class NextGreaterElem {
     public static void main(String[] args) {
-        int arr[]={5,7,1,2,6,0};
+        int arr[]={4,5,2,25};
 
         int arr2[] = nextGreaterElements(arr);
         System.out.println("The next greater elements are ");
