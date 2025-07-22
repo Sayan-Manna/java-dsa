1e56053ae60313b1131f2a51906ff1d024b9e28c
Sayan Manna <rexsayan028@gmail.com>
2025-07-22 21:02:01 +0530

    Auto-commit while IntelliJ active: 2025-07-22 21:02:01

--- StackAndQueue/Nge1.java
+++ StackAndQueue/Nge1.java
@@ -29,44 +29,45 @@ public class Nge1 {
             }
             stack.push(num);
         }
-        for (int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             nge[i] = map.getOrDefault(nums1[i], -1);
         }
         return nge;
 
         // Brute-force ---------------------------| O(n1 * n2)
-//        int n1 = nums1.length, n2=nums2.length;
-//        int[] nge = new int[n1];
-//
-//        // 1st check which number's nge we need to find out
-//        for (int i=0; i<n1; i++){
-//            int target = nums1[i];
-//            boolean found = false;
-//
-//            // find the nge for the target number
-//            for (int j=0; j<n2; j++) {
-//                if (nums2[j] == target) {
-//                    found = true; // we found the target number in nums2[], now find the nge
-//                    boolean hasNge = false;
-//
-//                    for (int k = j+1; k<n2; k++) {
-//                        if (nums2[k] > nums2[j]) {
-//                            hasNge = true;
-//                            nge[i] = nums2[k];
-//                            break;
-//                        }
-//                    }
-//                    if (!hasNge) nge[i] = -1;
-//                    break;
-//                }
-//            }
-//        }
-//        return nge;
+
+        // int n1 = nums1.length, n2=nums2.length;
+        // int[] nge = new int[n1];
+        //
+        // // 1st check which number's nge we need to find out
+        // for (int i=0; i<n1; i++){
+        // int target = nums1[i];
+        // boolean found = false;
+        //
+        // // find the nge for the target number
+        // for (int j=0; j<n2; j++) {
+        // if (nums2[j] == target) {
+        // found = true; // we found the target number in nums2[], now find the nge
+        // boolean hasNge = false;
+        //
+        // for (int k = j+1; k<n2; k++) {
+        // if (nums2[k] > nums2[j]) {
+        // hasNge = true;
+        // nge[i] = nums2[k];
+        // break;
+        // }
+        // }
+        // if (!hasNge) nge[i] = -1;
+        // break;
+        // }
+        // }
+        // }
+        // return nge;
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {4,1,2};
-        int[] nums2 = {1,3,4,2};
+        int[] nums1 = { 4, 1, 2 };
+        int[] nums2 = { 1, 3, 4, 2 };
 
         int[] res = nge1(nums1, nums2);
         System.out.println(Arrays.toString(res));
