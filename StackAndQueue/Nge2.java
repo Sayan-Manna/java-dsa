171515cb0df516f5a8b66419e084e6f41e0bfa01
Sayan Manna <rexsayan028@gmail.com>
2025-07-23 09:48:01 +0530

    Auto-commit while IntelliJ active: 2025-07-23 09:48:01

--- StackAndQueue/Nge2.java
+++ StackAndQueue/Nge2.java
@@ -1,4 +1,15 @@
 package StackAndQueue;
 
+import java.util.Arrays;
+
 public class Nge2 {
+    public static int[] nge2(int[] arr) {
+        
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {4, 1, 2};
+
+        int[] res = nge2(nums1);
+        System.out.println(Arrays.toString(res));
+    }
 }
