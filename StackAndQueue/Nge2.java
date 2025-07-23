746d6ec94a03d751031a77715f5b1a681f28dc92
Sayan Manna <rexsayan028@gmail.com>
2025-07-23 10:10:00 +0530

    Auto-commit while IntelliJ active: 2025-07-23 10:10:00

--- StackAndQueue/Nge2.java
+++ StackAndQueue/Nge2.java
@@ -5,7 +5,22 @@ import java.util.Arrays;
 public class Nge2 {
     public static int[] nge2(int[] arr) {
         // Brute-force - 2
-        
+        int n = arr.length;
+        int[] nge = new int[n];
+        for (int i=0; i<n-1; i++) {
+            boolean isPresent = false;
+            for (int j=i+1; j<i+n; j++) {
+                int idx = j % n;
+                if (arr[idx] > arr[i]) {
+                    nge[i] = arr[idx];
+                    isPresent = true;
+                    break;
+
+                }
+            }
+            if (!isPresent) nge[i] = -1;
+        }
+        return nge;
     }
     public static void main(String[] args) {
         int[] nums1 = {2,10,12,1,11};
