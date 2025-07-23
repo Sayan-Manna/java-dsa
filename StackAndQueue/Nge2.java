53acb470cf72c67cc9f5fc59f4ebb763d4babcdb
Sayan Manna <rexsayan028@gmail.com>
2025-07-23 19:49:00 +0530

    Auto-commit while IntelliJ active: 2025-07-23 19:49:00

--- StackAndQueue/Nge2.java
+++ StackAndQueue/Nge2.java
@@ -1,11 +1,19 @@
 package StackAndQueue;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Nge2 {
     public static int[] nge2(int[] arr) {
         // Optimal
-        
+        Stack<Integer> stack = new Stack<>();
+        int n = arr.length;
+        int[] nge = new int[n];
+        for (int i=2*n-1; i>=0; --i) {
+            boolean found = false;
+
+            
+        }
 
         // Brute-force - 2 -------| O(n^2) | O(n)
 //        int n = arr.length;
