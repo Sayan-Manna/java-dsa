772ff8d613742e94912faae0519560dcf83736d1
Sayan Manna <rexsayan028@gmail.com>
2025-07-24 01:14:01 +0530

    Auto-commit while IntelliJ active: 2025-07-24 01:14:01

--- StackAndQueue/NearestSmallerElement.java
+++ StackAndQueue/NearestSmallerElement.java
@@ -1,11 +1,24 @@
 package StackAndQueue;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class NearestSmallerElement {
     private static int[] nse(int[] nums) {
         // Optimal ---------|
-        
+        Stack<Integer> stack = new Stack<>();
+        int n = nums.length;
+        int[] nge = new int[n];
+        for (int i=0; i<n; i++) {
+            while (!stack.isEmpty() && arr[i] >= stack.peek()) {
+                stack.pop();
+            }
+            if (i < n) {
+                nge[i] = !stack.isEmpty() ? stack.peek() : -1;
+            }
+            stack.push(arr[i%n]);
+        }
+        return nge;
 
 
         // Brute-force
