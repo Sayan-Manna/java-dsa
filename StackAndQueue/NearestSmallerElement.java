684d01fad272ab80400a5fcdafc0f875c962a101
Sayan Manna <rexsayan028@gmail.com>
2025-07-24 00:41:01 +0530

    Auto-commit while IntelliJ active: 2025-07-24 00:41:01

--- StackAndQueue/NearestSmallerElement.java
+++ StackAndQueue/NearestSmallerElement.java
@@ -8,10 +8,16 @@ public class NearestSmallerElement {
         int n = nums.length;
         int[] nse = new int[n];
         for (int i=0; i<n; i++) {
+            boolean found = false;
             for (int j = i-1; i>=0; --i) {
-                if (nums[i])
+                if (nums[j] < nums[i]) {
+                    nse[i] = nums[j];
+                    found = true;
+                }
             }
+            if (!found) nse[i] = -1;
         }
+        return nse;
     }
 
     public static void main(String[] args) {
