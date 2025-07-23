85f5ca30a22a35c542724a0e36c488007aee7a6d
Sayan Manna <rexsayan028@gmail.com>
2025-07-24 00:28:00 +0530

    Auto-commit while IntelliJ active: 2025-07-24 00:28:00

--- StackAndQueue/NearestSmallerElement.java
+++ StackAndQueue/NearestSmallerElement.java
@@ -1,7 +1,16 @@
 package StackAndQueue;
 
+import java.util.Arrays;
+
 public class NearestSmallerElement {
-    public static void main(String[] args) {
+    private static int[] nse(int[] nums1) {
         
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {12,1,11};
+
+        int[] res = nse(nums1);
+        System.out.println(Arrays.toString(res));
     }
 }
