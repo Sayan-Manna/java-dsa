1f3b3013b938dd4f09a872b573f8102447d5bafd
Sayan Manna <rexsayan028@gmail.com>
2025-07-24 00:48:00 +0530

    Auto-commit while IntelliJ active: 2025-07-24 00:48:00

--- StackAndQueue/NearestSmallerElement.java
+++ StackAndQueue/NearestSmallerElement.java
@@ -4,25 +4,28 @@ import java.util.Arrays;
 
 public class NearestSmallerElement {
     private static int[] nse(int[] nums) {
+        // Optimal ---------|
+        
+
         // Brute-force
-        int n = nums.length;
-        int[] nse = new int[n];
-        for (int i=0; i<n; i++) {
-            boolean found = false;
-            for (int j = i-1; j>=0; --j) {
-                if (nums[j] < nums[i]) {
-                    nse[i] = nums[j];
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) nse[i] = -1;
-        }
-        return nse;
+//        int n = nums.length;
+//        int[] nse = new int[n];
+//        for (int i=0; i<n; i++) {
+//            boolean found = false;
+//            for (int j = i-1; j>=0; --j) {
+//                if (nums[j] < nums[i]) {
+//                    nse[i] = nums[j];
+//                    found = true;
+//                    break;
+//                }
+//            }
+//            if (!found) nse[i] = -1;
+//        }
+//        return nse;
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {12,1,11};
+        int[] nums1 = {4,5,2,10,8};
 
         int[] res = nse(nums1);
         System.out.println(Arrays.toString(res));
