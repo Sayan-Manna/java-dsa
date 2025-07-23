41f677e7d17d40514b48cd57ba4ce89fc63b0500
Sayan Manna <rexsayan028@gmail.com>
2025-07-23 10:16:00 +0530

    Auto-commit while IntelliJ active: 2025-07-23 10:16:00

--- StackAndQueue/Nge2.java
+++ StackAndQueue/Nge2.java
@@ -7,7 +7,7 @@ public class Nge2 {
         // Brute-force - 2
         int n = arr.length;
         int[] nge = new int[n];
-        for (int i=0; i<n-1; i++) {
+        for (int i=0; i<n; i++) {
             boolean isPresent = false;
             for (int j=i+1; j<i+n; j++) {
                 int idx = j % n;
