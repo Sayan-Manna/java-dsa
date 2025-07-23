c2e95a10ba0be2c0d49c83036793cab53f53ec7b
Sayan Manna <rexsayan028@gmail.com>
2025-07-23 10:07:00 +0530

    Auto-commit while IntelliJ active: 2025-07-23 10:07:00

--- StackAndQueue/Nge2.java
+++ StackAndQueue/Nge2.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public class Nge2 {
     public static int[] nge2(int[] arr) {
+        // Brute-force - 2
         
     }
     public static void main(String[] args) {
-        int[] nums1 = {4, 1, 2};
+        int[] nums1 = {2,10,12,1,11};
 
         int[] res = nge2(nums1);
         System.out.println(Arrays.toString(res));
