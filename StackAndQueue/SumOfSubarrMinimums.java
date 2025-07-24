70b8ad149eeb0f1b86a925ee18e899e5e0393c97
Sayan Manna <rexsayan028@gmail.com>
2025-07-25 00:57:01 +0530

    Auto-commit while IntelliJ active: 2025-07-25 00:57:01

--- StackAndQueue/SumOfSubarrMinimums.java
+++ StackAndQueue/SumOfSubarrMinimums.java
@@ -5,11 +5,20 @@ public class SumOfSubarrMinimums {
         // Optimal
         int mod = (int) (1e9 + 7);
         long sum = 0;
+        int[] nse = findNSE(arr);
+        int[] pse = findPSE(arr);
         
 
 
         // Brute-force
 
+    }
+
+    private static int[] findPSE(int[] arr) {
+    }
+
+    private static int[] findNSE(int[] arr) {
+        return null;
     }
 
     public static void main(String[] args) {
