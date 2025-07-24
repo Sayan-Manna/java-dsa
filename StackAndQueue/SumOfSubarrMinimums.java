74fbac1c20f745c16135a6f9e18de46d18f620d3
Sayan Manna <rexsayan028@gmail.com>
2025-07-25 01:04:01 +0530

    Auto-commit while IntelliJ active: 2025-07-25 01:04:00

--- StackAndQueue/SumOfSubarrMinimums.java
+++ StackAndQueue/SumOfSubarrMinimums.java
@@ -22,6 +22,17 @@ public class SumOfSubarrMinimums {
     }
 
     private static int[] findPSEE(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int n = arr.length;
+        int[] psee = new int[n];
+        for (int i=0; i<n; i++) {
+            while (!stack.isEmpty() && arr[i] < arr[stack.peek()]) {
+                stack.pop();
+            }
+            psee[i] = !stack.isEmpty() ? stack.peek() : -1;
+            stack.push(i);
+        }
+        return psee;
     }
 
     private static int[] findNSE(int[] arr) {
@@ -35,6 +46,7 @@ public class SumOfSubarrMinimums {
             nse[i] = stack.isEmpty() ? n : stack.peek();
             stack.push(i);
         }
+        return nse;
     }
 
     public static void main(String[] args) {
