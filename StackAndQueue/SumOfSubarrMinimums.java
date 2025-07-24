eba0cb7a8eeb922c634bdb06ddc814c425249509
Sayan Manna <rexsayan028@gmail.com>
2025-07-24 20:34:00 +0530

    Auto-commit while IntelliJ active: 2025-07-24 20:34:00

--- StackAndQueue/SumOfSubarrMinimums.java
+++ StackAndQueue/SumOfSubarrMinimums.java
@@ -1,8 +1,13 @@
 package StackAndQueue;
 
 public class SumOfSubarrMinimums {
+    private static int sumOfSubarrMins(int[] arr) {
+        // Brute-force
+        
+    }
+
     public static void main(String[] args) {
         int[] arr = {3,1,2,4};
-        
+        System.out.println(sumOfSubarrMins(arr));
     }
 }
