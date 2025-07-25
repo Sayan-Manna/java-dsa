020c7e8485c66ac17f75b1064b143d259d7e2528
Sayan Manna <rexsayan028@gmail.com>
2025-07-25 23:37:00 +0530

    Auto-commit while IntelliJ active: 2025-07-25 23:37:00

--- StackAndQueue/SumOfSubarrRanges.java
+++ StackAndQueue/SumOfSubarrRanges.java
@@ -1,6 +1,8 @@
 package StackAndQueue;
 
 
+import java.util.Stack;
+
 import static StackAndQueue.SumOfSubarrMinimums.sumOfSubarrMins;
 
 public class SumOfSubarrRanges {
@@ -17,12 +19,26 @@ public class SumOfSubarrRanges {
         int[] pge = findPGE(arr);
         for (int i=0; i<arr.length; i++) {
             long left = i - pge[i];
-            long right = nse[i] - i;
+            long right = ngee[i] - i;
             long contrib = ((left * right) % mod * arr[i]) % mod;
             sum = (sum + contrib) % mod;
         }
         return (int) sum;
     }
+    public static int[] findNGEE(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int n = arr.length;
+        int[] nse = new int[n];
+        for (int i = n-1; i>=0; --i) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+            nse[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+        return nse;
+    }
+
 
     public static void main(String[] args) {
         int[] arr = {4,-2,-3,4,1};
