eb6dcc2b11eaa75471068875285bcf1b3e17854e
Sayan Manna <rexsayan028@gmail.com>
2025-08-01 02:00:02 +0530

    Auto-commit while IntelliJ active: 2025-08-01 02:00:02

---
+++ StackAndQueue/RemoveKDigits.java
@@ -0,0 +1,54 @@
+package StackAndQueue;
+
+import java.util.Stack;
+
+public class RemoveKDigits {
+    private static String removeKDigits(String num, int k) {
+        Stack<Character> st = new Stack<>();
+        int n = num.length();
+        for(int i=0;i<n;i++){
+            //pop larger element from stack
+            while(!st.isEmpty() && k>0 && (st.peek() - '0' > num.charAt(i) - '0')){
+                st.pop();
+                k--;
+            }
+            //push smaller elements
+            st.push(num.charAt(i));
+        }
+        //if k is remaining pop k elements - "12345" k=3
+        while(k>0){
+            st.pop();
+            k--;
+        }
+        //after poping if stack is empty return zero
+        if(st.isEmpty()) return "0";
+        //if stack is not empty return result
+        String res = "";
+        while(!st.isEmpty()){
+            res += st.pop();
+        }
+        //remove any trailing zero from result as stack follow LIFO (so front zero are to be removed)
+        String result = "";
+        int ind;
+        for(ind=res.length()-1;ind>=0;ind--){
+            if(res.charAt(ind) != '0'){
+                break;
+            }
+        }
+        for(int i=ind;i>=0;i--){
+            result += res.charAt(i);
+        }
+        //if stack is empty after removing zeros -> return 0
+        if(result.isEmpty()) return "0";
+        return result;
+
+        // Brute-force - Recursion
+
+    }
+
+    public static void main(String[] args) {
+        String num = "1432219";
+        int k = 3;
+        System.out.println(removeKDigits(num,k));
+    }
+}
