9f897d2a818ed4062d2e7e806a148cf5dbd2e004
Sayan Manna <rexsayan028@gmail.com>
2025-08-07 00:00:01 +0530

    Auto-commit while IntelliJ active: 2025-08-07 00:00:01

--- StackAndQueue/LargestRectangleInHistogram.java
+++ StackAndQueue/LargestRectangleInHistogram.java
@@ -1,22 +1,29 @@
 package StackAndQueue;
 
+
+import java.util.Stack;
+
 public class LargestRectangleInHistogram {
 
     private static int largestRectangleArea(int[] arr) {
         // Optimal 2 -------------
+        
 
 
-        // Optimal 1 -------------
+        // Optimal 1 ------------- O(5N) | O(4N)
         /*
         * The intuition behind the approach is the same as finding the smaller element on both sides but in an optimized way
         * using the concept of the next greater element and the next smaller element.
         * [1,5,6,2] -> If I am at 5, the rectangle I can form taking 5 and 6. Formation stops at pse and nse
         */
         int maxArea = Integer.MIN_VALUE;
-
-
-
-
+        int[] pse = findPSE(arr);
+        int[] nse = findNSE(arr);
+        for (int i=0; i<arr.length; i++) {
+            int area = arr[i] * (nse[i]-pse[i]-1);
+            maxArea = Math.max(area, maxArea);
+        }
+        return maxArea;
 
         // Brute-force -------- O(N^2) | O(1)
 //        int maxArea = 0;
@@ -37,6 +44,39 @@ public class LargestRectangleInHistogram {
 //            maxArea = Math.max(area, maxArea);
 //        }
 //        return maxArea;
+    }
+
+    private static int[] findNSE(int[] heights) {
+        int n = heights.length;
+        int[] nse = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = n-1; i >=0; i--) {
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+                st.pop();
+            }
+
+            if (st.isEmpty()) nse[i] = n;
+            else nse[i] = st.peek();
+            st.push(i);
+        }
+        return nse;
+
+    }
+
+    private static int[] findPSE(int[] heights) {
+        int n = heights.length;
+        int[] pse = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+                st.pop();
+            }
+
+            if (st.isEmpty()) pse[i] = -1;
+            else pse[i] = st.peek();
+            st.push(i);
+        }
+        return pse;
     }
 
     public static void main(String[] args) {
