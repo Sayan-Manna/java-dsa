743754415d328a72fb92b64f14b011207ee311df
Sayan Manna <rexsayan028@gmail.com>
2025-08-06 22:00:01 +0530

    Auto-commit while IntelliJ active: 2025-08-06 22:00:01

--- StackAndQueue/LargestRectangleInHistogram.java
+++ StackAndQueue/LargestRectangleInHistogram.java
@@ -3,51 +3,40 @@ package StackAndQueue;
 public class LargestRectangleInHistogram {
 
     private static int largestRectangleArea(int[] arr) {
-        // Brute-force
-        int count = 0;
-        int total = 0;
-        for (int i=0; i<arr.length; i++) {
+        // Optimal 2 -------------
 
-            if (i==0) {
-                int idx = 0;
-                while (idx < arr.length && arr[idx+1] >= arr[i]) {
-                    count++;
-                    idx++;
-                }
-                if (idx > 0) {
-                    total += arr[i] * idx;
-                }
-            }else if (i==arr.length-1) {
-                int idx = arr.length-1;
-                while (idx >0 && arr[idx-1] >= arr[i]) {
-                    count++;
-                    idx--;
-                }
-                if (idx > 0) {
-                    total += arr[i] * idx;
-                }
-            }else {
-                int idx = 0;
-                while (idx < arr.length-1 && arr[idx+1] >= arr[i]) {
-                    count++;
-                    idx++;
-                }
-                if (idx > 0) {
-                    total += arr[i] * idx;
-                }
-                idx = arr.length-1;
-                while (idx >0 && arr[idx-1] >= arr[i]) {
-                    count++;
-                    idx--;
-                }
-                if (idx > 0) {
-                    total += arr[i] * idx;
-                }
 
-            }
+        // Optimal 1 -------------
+        /*
+        * The intuition behind the approach is the same as finding the smaller element on both sides but in an optimized way
+        * using the concept of the next greater element and the next smaller element.
+        * [1,5,6,2] -> If I am at 5, the rectangle I can form taking 5 and 6. Formation stops at pse and nse
+        */
+        int maxArea = Integer.MIN_VALUE;
 
-        }
-        return total;
+
+
+
+
+        // Brute-force -------- O(N^2) | O(1)
+//        int maxArea = 0;
+//        for (int i=0; i<arr.length; i++) {
+//            // Expand left -> when i=0, calc expanding right only so left > 0
+//            int left = i;
+//            while (left > 0 && arr[left-1] >= arr[i]) {
+//                left--;
+//            }
+//            // Expand right -> when i=n-1, calc expanding left only so right < n-1
+//            int right = i;
+//            while (right < arr.length-1 && arr[right+1] >= arr[i]) {
+//                right++;
+//            }
+//
+//            int width = right - left + 1;
+//            int area = arr[i] * width;
+//            maxArea = Math.max(area, maxArea);
+//        }
+//        return maxArea;
     }
 
     public static void main(String[] args) {
