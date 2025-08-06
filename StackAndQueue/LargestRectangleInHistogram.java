c967beae41028698bcde6933bac5e8a30ffd6469
Sayan Manna <rexsayan028@gmail.com>
2025-08-06 20:00:01 +0530

    Auto-commit while IntelliJ active: 2025-08-06 20:00:01

--- StackAndQueue/LargestRectangleInHistogram.java
+++ StackAndQueue/LargestRectangleInHistogram.java
@@ -4,15 +4,50 @@ public class LargestRectangleInHistogram {
 
     private static int largestRectangleArea(int[] arr) {
         // Brute-force
+        int count = 0;
+        int total = 0;
         for (int i=0; i<arr.length; i++) {
 
-            for (int j=0; j<=i; j++) {
-                if (arr[j] >= arr[i]) {
-                    
+            if (i==0) {
+                int idx = 0;
+                while (idx < arr.length && arr[idx+1] >= arr[i]) {
+                    count++;
+                    idx++;
                 }
+                if (idx > 0) {
+                    total += arr[i] * idx;
+                }
+            }else if (i==arr.length-1) {
+                int idx = arr.length-1;
+                while (idx >0 && arr[idx-1] >= arr[i]) {
+                    count++;
+                    idx--;
+                }
+                if (idx > 0) {
+                    total += arr[i] * idx;
+                }
+            }else {
+                int idx = 0;
+                while (idx < arr.length-1 && arr[idx+1] >= arr[i]) {
+                    count++;
+                    idx++;
+                }
+                if (idx > 0) {
+                    total += arr[i] * idx;
+                }
+                idx = arr.length-1;
+                while (idx >0 && arr[idx-1] >= arr[i]) {
+                    count++;
+                    idx--;
+                }
+                if (idx > 0) {
+                    total += arr[i] * idx;
+                }
+
             }
 
         }
+        return total;
     }
 
     public static void main(String[] args) {
