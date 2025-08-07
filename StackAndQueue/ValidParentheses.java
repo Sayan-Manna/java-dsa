d59e033259f5b46205e0fdf6e04f069e4ee430ed
Sayan Manna <rexsayan028@gmail.com>
2025-08-08 00:00:03 +0530

    Auto-commit while IntelliJ active: 2025-08-08 00:00:03

--- StackAndQueue/ValidParentheses.java
+++ StackAndQueue/ValidParentheses.java
@@ -2,7 +2,38 @@ package StackAndQueue;
 
 public class ValidParentheses {
     private static boolean isValidParentheses(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i=0; i<s.length(); i++) {
 
+            if (s.charAt(i) =='(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                st.push(s.charAt(i));
+            }else {
+                if (st.isEmpty()) return false;
+
+                char ch = st.peek();
+                st.pop();
+
+                if (s.charAt(i) == ')') {
+                    char popped = st.pop();
+                    if (popped != '(') {
+                        break;
+                    }
+                }
+                if (s.charAt(i) == ']') {
+                    char popped = st.pop();
+                    if (popped != '[') {
+                        break;
+                    }
+                }
+                if (s.charAt(i) == '}') {
+                    char popped = st.pop();
+                    if (popped != '{') {
+                        break;
+                    }
+                }
+            }
+        }
+        return (st.isEmpty()) ? true:false;
     }
 
     public static void main(String[] args) {
