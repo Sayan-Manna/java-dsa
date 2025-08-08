55d03aab7571c023a1a11f4ad2a1a7b3a23fb867
Sayan Manna <rexsayan028@gmail.com>
2025-08-09 02:00:01 +0530

    Auto-commit while IntelliJ active: 2025-08-09 02:00:00

---
+++ StackAndQueue/Impl/QueueImplUsingLL.java
@@ -0,0 +1,7 @@
+package StackAndQueue.Impl;
+
+public class QueueImplUsingLL {
+    public static void main(String[] args) {
+
+    }
+}
--- StackAndQueue/Impl/QueueUsingStacks.java
+++ StackAndQueue/Impl/QueueUsingStacks.java
@@ -3,15 +3,44 @@ package StackAndQueue.Impl;
 import java.util.Stack;
 
 class MyQueue {
+
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+    public MyQueue() {}
     // Approach 2 : O(1) | O(2N)
+    void push(int x) {
+        // #2 : x -> s1
+        stack1.push(x);
+    }
+    int pop() {
+        if (stack2.isEmpty()) {
+            // s1 -> s2
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+    int peek() {
+        if (stack2.isEmpty()) {
+            // s1 -> s2
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+    /*
+    * push(1);  // stack1: [1], stack2: []
+    * push(2);  // stack1: [1, 2], stack2: []
+    * size();   // returns 0 ❌ but actually queue size = 2
+    */
+    int size() {
+        return stack2.size() + stack1.size();
+    }
 
 
     // Approahc 1 : O(2N) in push | O(2N) SC
-//    Stack<Integer> stack1 = new Stack<>();
-//    Stack<Integer> stack2 = new Stack<>();
-//
-//    public MyQueue() {}
-//
 //    void push(int x) {
 //        // #1 : s1 -> s2
 //        while (!stack1.isEmpty()){
