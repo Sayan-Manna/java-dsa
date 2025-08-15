0d3b0f1d82b45c7258c35e31162ff4dde7f49362
Sayan Manna <rexsayan028@gmail.com>
2025-08-16 04:00:09 +0530

    Auto-commit while IntelliJ active: 2025-08-16 04:00:09

---
+++ StackAndQueue/CarFleet.java
@@ -0,0 +1,17 @@
+package StackAndQueue;
+
+public class CarFleet {
+
+  private static int carFleet(int target, int[] position, int[] speed) {
+
+  }
+
+  public static void main(String[] args) {
+    int target = 12;
+    int[] position = { 10, 8, 0, 5, 3 };
+    int[] speed = { 2, 4, 1, 1, 3 };
+    System.out.println(carFleet(target, position, speed));
+
+  }
+
+}
