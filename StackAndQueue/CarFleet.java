d8a55d961e6ddbfde35c591fdf9378fd5ef8d4d8
Sayan Manna <rexsayan028@gmail.com>
2025-08-17 02:00:01 +0530

    Auto-commit while IntelliJ active: 2025-08-17 02:00:01

--- StackAndQueue/CarFleet.java
+++ StackAndQueue/CarFleet.java
@@ -5,26 +5,32 @@ import java.util.Arrays;
 public class CarFleet {
 
   private static int carFleet(int target, int[] position, int[] speed) {
-    // Brute-force
+
+    // --------------------| O(NlogN) |
     int n = position.length;
     if (n == 0)
       return 0;
 
-    // Pair of position and speed to reach target
-    double[][] cars = new double[n][2];
+    Integer[] indices = new Integer[n];
+    /* [0,1,2,3...] */
     for (int i = 0; i < n; i++) {
-      cars[i][0] = position[i];
-      cars[i][1] = (double) (target - position[i]) / speed[i];
+      indices[i] = i;
     }
+
     // Sort by position closer to target first
-    Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+    /*
+     * [0,1,3,4,2] -> so order is : pos = 10, pos = 8, ....
+     */
+    Arrays.sort(indices, (a, b) -> Integer.compare(position[b], position[a]));
 
     int fleets = 0;
     double maxTime = 0;
 
-    // Traverse from closest to furtheest
+    // Traverse from closest to furthest
     for (int i = 0; i < n; i++) {
-      double time = cars[i][1];
+      int index = indices[i];
+      double time = (double) (target - position[i]) / speed[index];
+
       // If the car is taking longerm it forms a new fleet
       if (time > maxTime) {
         fleets++;
@@ -33,6 +39,41 @@ public class CarFleet {
       // Else it joins the existing fleet
     }
     return fleets;
+
+    // Brute --------------------| O(NlogN + 2N) | O(2N) : cars array of size N
+    // int n = position.length;
+    // if (n == 0)
+    // return 0;
+
+    // // Pair of position and speed to reach target
+    /*
+     * [[10,1.0], [8,1.0],[0,12.0],....]
+     */
+    // double[][] cars = new double[n][2];
+    // for (int i = 0; i < n; i++) {
+    // cars[i][0] = position[i];
+    // cars[i][1] = (double) (target - position[i]) / speed[i];
+    // }
+    // // Sort by position closer to target first
+    /*
+     * [[10,1.0],[8,1.0],[5,7.0],...]
+     */
+    // Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+
+    // int fleets = 0;
+    // double maxTime = 0;
+
+    // // Traverse from closest to furtheest
+    // for (int i = 0; i < n; i++) {
+    // double time = cars[i][1];
+    // // If the car is taking longerm it forms a new fleet
+    // if (time > maxTime) {
+    // fleets++;
+    // maxTime = time;
+    // }
+    // // Else it joins the existing fleet
+    // }
+    // return fleets;
   }
 
   public static void main(String[] args) {
---
+++ StackAndQueue/CarFleet2.java
@@ -0,0 +1,8 @@
+package StackAndQueue;
+
+class CarFleet2 {
+  public static void main(String[] args) {
+
+  }
+
+}
