980767b3396cfb17ee330dd36456613daa06af75
Sayan Manna <rexsayan028@gmail.com>
2025-08-27 18:00:00 +0530

    Auto-commit while IntelliJ active: 2025-08-27 18:00:00

--- Recursion/Pow.java
+++ Recursion/Pow.java
@@ -3,7 +3,7 @@ package Recursion;
 public class Pow {
   public static double myPow(double x, int n) {
 
-    // Optimnal ----- O(log|N|) time complexity and O(1) space complexity
+    // Optimnal ----- O(log|N|) time complexity and O(log|N|) space complexity
     // if (n == 0) {
     // return 1.0;
     // }
@@ -17,6 +17,23 @@ public class Pow {
     // return half * half / x;
     // }
     // }
+
+    // Optimal Iterative ----- O(log|N|) time complexity and O(1) space complexity
+    long exp = n;
+    if (exp < 0) {
+      x = 1 / x;
+      exp = -exp;
+    }
+    double ans = 1.0;
+    while (exp > 0) {
+      // if exponent is odd
+      if ((exp & 1) == 1) {
+        ans = ans * x;
+      }
+      x = x * x; // square the base
+      exp = exp >> 1; // divide the exponent by 2
+    }
+    return ans;
 
     // Brute-force ---- O(|N|) time complexity and O(1) space complexity
     // if (n == 0)
@@ -35,7 +52,7 @@ public class Pow {
 
   public static void main(String[] args) {
     double x = 2.00000;
-    int n = 3;
+    int n = 13;
     System.out.println(myPow(x, n));
 
   }
