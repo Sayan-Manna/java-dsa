f675a55047d0da9a7121f34632fb297924f0e496
Sayan Manna <rexsayan028@gmail.com>
2025-12-16 22:00:01 +0530

    Auto-commit while IntelliJ active: 2025-12-16 22:00:01

--- stream/AvgMinMax.java
+++ stream/AvgMinMax.java
@@ -2,6 +2,8 @@ package stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
 
 // Find the average salary of employees.
 
@@ -14,13 +16,26 @@ public class AvgMinMax {
         );
 
         double avgSal = employees.stream()
-                .map(emp -> emp.salary)
-                .mapToDouble(sal -> sal)
+                .mapToDouble(e -> e.salary)
                 .average()
                 .orElse(0.0);
 
-        System.out.println(avgSal);
+        OptionalInt highestSal = employees.stream()
+                        .mapToInt(e ->  (int) e.salary)
+                        .max();
 
+        int secondHighestSal = employees.stream()
+                        .mapToInt(e -> (int) e.salary)
+                        .boxed() // Since IntStream.sorted() doesn't accept comparator
+                        .distinct()
+                        .sorted((a,b)->b-a) // Instream.sorted() doesn't accept comparator -> only natural order sorted()
+                        .skip(1)
+                        .findFirst()
+                        .orElse(0);
+
+        System.out.println(avgSal);
+        System.out.println(highestSal.orElse(0));
+        System.out.println(secondHighestSal);
     }
 }
 
--- stream/CharFreqInStr.java
+++ stream/CharFreqInStr.java
@@ -1,8 +1,33 @@
 package stream;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class CharFreqInStr {
     public static void main(String[] args) {
         String str = "programming";
-         
+
+        Map<Character, Long> freq = str.chars()
+                .mapToObj(ch -> (char) ch) // Converts the IntStream to Stream<Character>
+                .collect(Collectors.groupingBy(
+                        ch -> ch, // (identity) -> group by the character itself
+                        Collectors.counting() // Count occurrences in each group
+                ));
+
+        System.out.println(freq);
+
     }
 }
+
+/*
+
+** str.chars()
+* Returns an IntStream of the string’s UTF-16 code units.
+* Each element is an int representing a char value (0 – 65535).
+* Good for “normal” ASCII/most BMP characters.
+* Limitation: for characters outside the BMP (like many emojis), they’re represented by surrogate pairs, so chars() will treat them as two separate values.
+
+---
+
+* counting() returns Long
+*/
