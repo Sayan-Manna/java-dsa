1ae89343713c168637843e039f4df65f1ea0fa90
Sayan Manna <rexsayan028@gmail.com>
2025-12-17 00:00:01 +0530

    Auto-commit while IntelliJ active: 2025-12-17 00:00:01

---
+++ stream/EmplDept.java
@@ -0,0 +1,15 @@
+package stream;
+
+public class EmplDept {
+    public static void main(String[] args) {
+
+    }
+}
+class Employee {
+    String dept;
+
+    Employee(String dept) {
+        this.dept = dept;
+    }
+    
+}
